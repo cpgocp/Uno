7d1f432d30d065603b60740817535a9d1813f91a
cjp8426 <cjp8426@g.rit.edu>
2022-03-05 14:12:26 -0500

    Added switch case to Card and method to get starting letter of color, added creation of deck to game and started working on shuffling deck

--- src/Game/Card.java
+++ src/Game/Card.java
@@ -4,13 +4,33 @@ public class Card {
     private String color;
     private int num;
 
-    public Card (String c, int n){
-        color = c;
+    public Card (int c, int n){
+        switch(c){
+            case 0:
+                color = "Red";
+                break;
+            case 1:
+                color = "Blue";
+                break;
+            case 2:
+                color = "Green";
+                break;
+            case 3:
+                color = "Yellow";
+                break;
+            case 4:
+                color = "Wild";
+                break;
+        }
         num = n;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public Character getLetter() {
+        return color.charAt(0);
     }
 
     public int getNum() {
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,6 +1,7 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
     private ArrayList<Card> Deck;
@@ -8,12 +9,26 @@ public class Game {
 
     private static int HAND_SIZE = 7;
 
+    Random random = new Random();
+
     public Game(){
         for(int i = 0; i < 4; i++){ //4 suits
             for(int j = 0; j < 13 ; j++ ){ //colored cards
-
+                Card card1 = new Card(i,j);
+                Card card2 = new Card(i,j);
+                Deck.add(card1);
+                Deck.add(card2);
             }
+            Card card3 = new Card(4,0);
+            Card card4 = new Card(4,4);
+            Deck.add(card3);
+            Deck.add(card4);
+        }
+
+        for(int i = 0; i < Deck.size(); i++){
+
         }
     }
+
 
 }
